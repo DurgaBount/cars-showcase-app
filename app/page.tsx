0ebb2722ebb41f4f5731734a38c7783815fdcaf9
DurgaBount <durga.prasad@bounteous.com>
2024-01-17 09:09:05 +0530

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default async function Home({ searchParams }: any) {
           </section>
         ) : (
           <div className="home__error-container">
-            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <h2 className="text-black text-xl font-bold">Oops, no resultss</h2>
             <p>{allCars?.message}</p>
           </div>
         )}
